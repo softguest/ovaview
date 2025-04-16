a5efaaa2f16e33135179ff23b4417aeaca7bed83
borishitz <borishitz@gmail.com>
2025-04-16 07:20:25 +0100

    connect prisma to mongo

--- app/page.tsx
+++ app/page.tsx
@@ -15,28 +15,11 @@ export default function LoginPage() {
   return (
     <main className="flex flex-col">
         <section className=''>
-          <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2'>
-              <div className='flex justify-center items-center'>
-                <div className="space-y-4 px-4 md:px-32 text-center hidden md:block">
-                  <center>
-                      <Image className="" src={Logo} width={170} height={170} alt=""/>
-                  </center>
-                    <h1 className={cn(
-                              "text-4xl font-semibold text-sky-600 drop-shadow-md",
-                              font.className,
-                            )}>
-                              Get Started!
-                    </h1>
-                    <h4 className="font-semibold text-2xl text-sky-600 ">A Gateway To An Easy Student Admition Portal With Records Tracking.</h4>
-                    <p className=" text-[16px] text-slate-500">Tired of paper forms, endless queues, & The Hassle Of Traditional School Registration?  Yoollaa Is Here To Revolutionize The Way You Apply & Get Admited into any School!</p>
-                </div>
-              </div>
-              <div className=' bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-white to-slate-200 flex justify-center items-center py-16'>
-                  <div className="space-y-6 text-center py-4 md:px-20">
-                      <LoginForm />
-                  </div> 
-              </div>
-          </div>
+            <div className=' bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-white to-slate-200 flex justify-center items-center py-16'>
+                <div className="space-y-6 text-center py-4 md:px-20">
+                    <LoginForm />
+                </div> 
+            </div>
           <AboutSec />
         </section>
     </main>
--- components/aboutSec/AboutSec.tsx
+++ components/aboutSec/AboutSec.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 const AboutSec = () => {
   return (
-      <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-orange to-sky-400 text-slate-700 text-center py-24">
+      <div className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-orange-100 to-slate-400 text-slate-700 text-center py-24">
           <div className="space-y-4 px-4">
             <h1 className="text-4xl font-bold">How Weekly Works.</h1>
             <div className="text-1xl pb-4"><em>To See The Full Picture Of Weekly & Understand Its Core Features.</em></div>
